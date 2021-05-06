82a270822303a35c2abc4b3d67459bd5176b800c
Lautaro <nievesp.lautaro@hotmail.com>
2021-05-06 19:36:10 -0300

    Start up release

---
+++ src/main/java/com/TpAvanzada/Trabajo/Final/Avanzada/TrabajoFinalAvanzadaApplication.java
@@ -0,0 +1,13 @@
+package com.TpAvanzada.Trabajo.Final.Avanzada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoFinalAvanzadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoFinalAvanzadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TpAvanzada/Trabajo/Final/Avanzada/TrabajoFinalAvanzadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TpAvanzada.Trabajo.Final.Avanzada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoFinalAvanzadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
